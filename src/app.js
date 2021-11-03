f640b4c6ef243e7d05cd9079f886fc152bb803ed
mac-book <lexx.mg@gmail.com>
2021-11-03 14:54:33 +0300

    sunc

--- src/app.js
+++ src/app.js
@@ -2,20 +2,20 @@
 'use strict';
 
 import { upload } from './upload';
-import { initializeApp } from "firebase/app";
-import { getStorage, ref, uploadBytes, getMetadata, updateMetadata } from "firebase/storage";
+//import { initializeApp } from "firebase/app";
+//import { getStorage, ref, uploadBytes, getMetadata, updateMetadata } from "firebase/storage";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBCppba0e8Uql3bqhpDhEMZ3O8kButZ3JA",
-  authDomain: "fe-uplode-images.firebaseapp.com",
-  projectId: "fe-uplode-images",
-  storageBucket: "fe-uplode-images.appspot.com",
-  messagingSenderId: "222384173060",
-  appId: "1:222384173060:web:b1c41ac5e208a462e60551"
-};
-
-const firebaseApp = initializeApp(firebaseConfig);
-const storage = getStorage();
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBCppba0e8Uql3bqhpDhEMZ3O8kButZ3JA",
+//   authDomain: "fe-uplode-images.firebaseapp.com",
+//   projectId: "fe-uplode-images",
+//   storageBucket: "fe-uplode-images.appspot.com",
+//   messagingSenderId: "222384173060",
+//   appId: "1:222384173060:web:b1c41ac5e208a462e60551"
+// };
+//
+// const firebaseApp = initializeApp(firebaseConfig);
+// const storage = getStorage();
 
 // console.log(storage);
 // console.log(app);
@@ -24,34 +24,54 @@ const storage = getStorage();
 upload('.card__input', {
   multi: true,
   accept: ['.gif', '.jpg'],
-  onUpload
+  onUpload(files) {
+    const formData = new FormData();
+    formData.append('upload', 1);
+
+    files.forEach((item, i) => {
+      formData.append('file[]', files[i]);
+    });
+    //console.log([...files]);
+
+    fetch('http://photo-gallery.my/php/set-files.php', {
+      method: 'POST',
+      body: formData
+    }).then(res => {
+      if (!res.ok) {
+        throw new Error(`Ошибка ${url} ${res}`);
+      } else {
+        return res;
+      }
+    }).then(res => res.json() )
+      .then(data => console.log(data));
+    }
 });
 
 
-function onUpload(files) {
-  files.forEach((item, i) => {
-    const spaceRef = ref(storage, `images/${item.name}`);
-
-    uploadBytes(spaceRef, item).then((snapshot) => {
-      console.log(snapshot);
-      console.log('Uploaded a blob or file!');
-    });
-
-    getMetadata(spaceRef).then(metadata => {
-      console.log({...metadata});
-    });
-
-    const newMetadata = {
-      cacheControl: 'public,max-age=300',
-      contentType: 'image/jpeg'
-    };
-
-    updateMetadata(spaceRef, newMetadata)
-      .then((metadata) => {
-        console.log(metadata.size);
-        // Updated metadata for 'images/forest.jpg' is returned in the Promise
-      }).catch((error) => {
-        // Uh-oh, an error occurred!
-      });
-  });
-}
+// function onUpload(files) {
+//   files.forEach((item, i) => {
+//     const spaceRef = ref(storage, `images/${item.name}`);
+//
+//     uploadBytes(spaceRef, item).then((snapshot) => {
+//       console.log(snapshot);
+//       console.log('Uploaded a blob or file!');
+//     });
+//
+//     getMetadata(spaceRef).then(metadata => {
+//       console.log({...metadata});
+//     });
+//
+//     const newMetadata = {
+//       cacheControl: 'public,max-age=300',
+//       contentType: 'image/jpeg'
+//     };
+//
+//     updateMetadata(spaceRef, newMetadata)
+//       .then((metadata) => {
+//         console.log(metadata.size);
+//         // Updated metadata for 'images/forest.jpg' is returned in the Promise
+//       }).catch((error) => {
+//         // Uh-oh, an error occurred!
+//       });
+//   });
+// }
